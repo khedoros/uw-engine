96e409dff96a2509d99484ee94369544afeeda75
khedoros <khedoros@gmail.com>
2017-05-08 23:25:13 -0700

    uw_model class changes
    Started preparing for a more complete class implementation, so that I
    can dump out a vector that represents a model to draw

--- uw_model.h
+++ uw_model.h
@@ -13,18 +13,23 @@ class uw_model {
 
     class point {
         float x,y,z;
-        color c; //rgba from util
         float u,v;
-        bool variable; //Some points are "variable height", reaching all the way to the ceiling, etc.
+        color c; //rgba from util
+        int vertno; //vertex number, as separate from its index in an array of vertices for a model or face
+        bool var_height; //Some points are "variable height", reaching all the way to the ceiling, etc.
     };
 
+    class face {
+        bool goraud;
+        std::vector<point> points;
+        float nx, ny, nz; //normal vector
+    };
     std::vector<point> points;
     std::vector<std::vector<size_t>> faces;
 
-    //private:
+    private:
     bool check_offset(uint32_t, std::ifstream&);
     int skip_data(std::ifstream&);
     static const uint32_t start_offsets[];
     static const uint32_t model_table_offsets[];
-
 };
