70071dd191272f2e78346acef5f43863f86bfe1a
khedoros <khedoros@gmail.com>
2021-12-04 10:06:36 -0700

    Fix tabs/spaces

--- mzBinary.cpp
+++ mzBinary.cpp
@@ -8,10 +8,10 @@
 using namespace std;
 
 void mzBinary::printHeaderInfo() {
-    cout<<"Filename: "<<fileName<<endl<<"Filesize: "<<fileSize<<endl;
-    printf("Bytes on last page: %04x\n"
-	       "Total pages: %04x\n"
-	       "Relocation entries: %d\n"
+	cout<<"Filename: "<<fileName<<endl<<"Filesize: "<<fileSize<<endl;
+	printf("Bytes on last page: %04x\n"
+		   "Total pages: %04x\n"
+		   "Relocation entries: %d\n"
 		   "Header size in paragraphs: %04x\n"
 		   "Min mem needed after exe: %04x\n"
 		   "Max mem needed after exe: %04x\n"
@@ -22,67 +22,67 @@ void mzBinary::printHeaderInfo() {
 		   "Offset of relocation table: %04x\n"
 		   "Overlay number (expect 0): %04x\n"
 		   "Could be ID: %02x %02x %02x %02x\n",header.last_page_bytes, header.total_pages, header.reloc_entries,
-		                                        header.header_para_size, header.min_paras_after_code, header.max_paras_after_code,
+												header.header_para_size, header.min_paras_after_code, header.max_paras_after_code,
 												header.stack_seg_offset, header.initial_sp,header.chksum, header.init_cs,header.init_ip,
-												header.reloc_offset,	header.overlay_num,
+												header.reloc_offset, header.overlay_num,
 												header.poss_id[0], header.poss_id[1], header.poss_id[2], header.poss_id[3]);
-    if(header.poss_id[0] == 0x01 && header.poss_id[1] == 0x00 && header.poss_id[2] == 0xfb) {
-        std::cout<<"Probably Borland TLink, version "<<header.poss_id[3]/16<<endl;
-    }
-    cout<<"Relocation table: "<<header.reloc_entries<<" entries"<<endl;
-    bool odd=false;
-    uint16_t offset=0;
-    for(auto off:relocs) {
+	if(header.poss_id[0] == 0x01 && header.poss_id[1] == 0x00 && header.poss_id[2] == 0xfb) {
+		std::cout<<"Probably Borland TLink, version "<<header.poss_id[3]/16<<endl;
+	}
+	cout<<"Relocation table: "<<header.reloc_entries<<" entries"<<endl;
+	bool odd=false;
+	uint16_t offset=0;
+	for(auto off:relocs) {
 		printf("%04x:%04x\n",off.first,off.second);
-    }
+	}
 }
 
 mzBinary::mzBinary(const std::string& filename) : fileName(filename) {
-    assert(sizeof(mz_header) == 32);
-    ifstream in;
-    in.open(filename.c_str());
-    in.seekg(0,ios::end);
-    size_t size = in.tellg();
+	assert(sizeof(mz_header) == 32);
+	ifstream in;
+	in.open(filename.c_str());
+	in.seekg(0,ios::end);
+	size_t size = in.tellg();
 	fileSize = size;
-    in.seekg(0,ios::beg);
-    in.read(reinterpret_cast<char *>(&header), sizeof(mz_header));
-    in.seekg(header.reloc_offset,ios::beg);
-    uint16_t reloc_entry[2];
-    for(int i=0;i<header.reloc_entries;i++) {
-        in.read(reinterpret_cast<char *>(&(reloc_entry[0])), 2 * sizeof(uint16_t));
-    	relocs.emplace(relocs.end(), std::make_pair(reloc_entry[0],reloc_entry[1]));
-    }
+	in.seekg(0,ios::beg);
+	in.read(reinterpret_cast<char *>(&header), sizeof(mz_header));
+	in.seekg(header.reloc_offset,ios::beg);
+	uint16_t reloc_entry[2];
+	for(int i=0;i<header.reloc_entries;i++) {
+		in.read(reinterpret_cast<char *>(&(reloc_entry[0])), 2 * sizeof(uint16_t));
+		relocs.emplace(relocs.end(), std::make_pair(reloc_entry[0],reloc_entry[1]));
+	}
 
-    size_t binarySize = (header.total_pages - 1) * 512 + header.last_page_bytes;
-    in.seekg(16 * header.header_para_size, ios::beg);
-    main_binary.resize(binarySize);
-    in.read(reinterpret_cast<char *>(main_binary.data()), binarySize);
+	size_t binarySize = (header.total_pages - 1) * 512 + header.last_page_bytes;
+	in.seekg(16 * header.header_para_size, ios::beg);
+	main_binary.resize(binarySize);
+	in.read(reinterpret_cast<char *>(main_binary.data()), binarySize);
 
-    // findOverlayTable
-    // read overlay descriptions into overlays, into ovrDesc
-    // findOverlayDataBase
-    // Use the table and base address to iterate through the overlays, reading them into "overlays" "overlayBinary" and "overlayRelocs" fields
-    in.close(); 
+	// findOverlayTable
+	// read overlay descriptions into overlays, into ovrDesc
+	// findOverlayDataBase
+	// Use the table and base address to iterate through the overlays, reading them into "overlays" "overlayBinary" and "overlayRelocs" fields
+	in.close(); 
 }
 
 size_t mzBinary::findOverlayTable() {
-    size_t location = 0;
-    for(int i = 0; i < main_binary.size(); i+=0x10) {
-        
-    }
-    return location;
+	size_t location = 0;
+	for(int i = 0; i < main_binary.size(); i+=0x10) {
+		
+	}
+	return location;
 }
 
 size_t mzBinary::findOverlayDataBase() {
-    return 0;
+	return 0;
 }
 
 size_t mzBinary::findOverlayBinary(int overlay) {
-    return 0;
+	return 0;
 }
 
 size_t mzBinary::findOverlayRelocs(int overlay) {
-    return 0;
+	return 0;
 }
 
 void mzBinary::dumpBinaryToFile(int index, std::string& filename, bool clearReloc) {}
--- mzBinary.h
+++ mzBinary.h
@@ -3,41 +3,41 @@
 #include<utility>
 
 typedef struct {
-    char magic[2];
-    uint16_t last_page_bytes;
-    uint16_t total_pages;
-    uint16_t reloc_entries;
-    uint16_t header_para_size;
-    uint16_t min_paras_after_code;
-    uint16_t max_paras_after_code;
-    uint16_t stack_seg_offset;
-    uint16_t initial_sp;
-    uint16_t chksum;
-    uint16_t init_ip;
-    uint16_t init_cs;
-    uint16_t reloc_offset;
-    uint16_t overlay_num;
-    uint8_t poss_id[4];
+	char magic[2];
+	uint16_t last_page_bytes;
+	uint16_t total_pages;
+	uint16_t reloc_entries;
+	uint16_t header_para_size;
+	uint16_t min_paras_after_code;
+	uint16_t max_paras_after_code;
+	uint16_t stack_seg_offset;
+	uint16_t initial_sp;
+	uint16_t chksum;
+	uint16_t init_ip;
+	uint16_t init_cs;
+	uint16_t reloc_offset;
+	uint16_t overlay_num;
+	uint8_t poss_id[4];
 } mz_header;
 
 typedef struct {
-    uint8_t int_code[2];        // Overlay manager interrupt
-    uint16_t memswap;           // Runtime memory swap address
-    uint32_t fileoff;           // Offset in the file to the code
-    uint16_t codesize;          // Code size
-    uint16_t relsize;           // Relocation area size
-    uint16_t nentries;          // Number of overlay entries
-    uint16_t prevstuboff;       // Previous stub
-    uint16_t prevstubseg;
-    uint16_t nextstuboff;
-    uint16_t nextstubseg;
+	uint8_t int_code[2];    // Overlay manager interrupt
+	uint16_t memswap;       // Runtime memory swap address
+	uint32_t fileoff;       // Offset in the file to the code
+	uint16_t codesize;      // Code size
+	uint16_t relsize;       // Relocation area size
+	uint16_t nentries;      // Number of overlay entries
+	uint16_t prevstuboff;   // Previous stub
+	uint16_t prevstubseg;
+	uint16_t nextstuboff;
+	uint16_t nextstubseg;
 } overlay_description;
 
 typedef struct {
-    int overlayIndex;
-    overlay_description ovrDesc;
-    std::vector<uint8_t> overlayBinary;
-    std::vector<uint16_t> overlayRelocs;
+	int overlayIndex;
+	overlay_description ovrDesc;
+	std::vector<uint8_t> overlayBinary;
+	std::vector<uint16_t> overlayRelocs;
 } overlay_data;
 
 class mzBinary {
