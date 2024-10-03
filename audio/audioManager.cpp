c9e5dd7bf0b4ae5229c187333ed5ca2b404b829e
khedoros <khedoros@gmail.com>
2024-10-02 21:23:46 -0700

    First working version of audioManager
    - Inits SDL audio+timer, starts generating audio from opl using callback. Accepts commands to play an xmi (no queueing, though) and to play OPL sfx.

--- audio/audioManager.cpp
+++ audio/audioManager.cpp
@@ -1,23 +1,67 @@
 #include<string>
+#include<SDL2/SDL.h>
 
+#include "audioManager.h"
 #include "opl/superOpl.h"
+#include "oplSequencer.h"
 
-class audioManager {
-public:
-    audioManager::audioManager(std::string& adFile, std::string& dataPath, std::string& dataPath2);
-    void audioManager::playMusic(std::string& xmiFile); // Stop current queue, play music
-    void audioManager::playWav(std::string& vocFile);
-    void audioManager::playSfx(int number);
-    void audioManager::enqueueSong(std::string& xmiFile);
-    void audioManager::clearQueue();
-    void audioManager::startMusic();
-    void audioManager::stopMusic();
-    void audioManager::muteMusic();
-    void audioManager::restartMusic();
-    void audioManager::setVol(int level);
-private:
-    int volumeLevel;
-    superOpl opl;
-    bool runMusic;
-    bool runSfx;
-};
+Uint32 AudioCallback(Uint32 interval, void* oplPtr);
+
+audioManager::audioManager(const std::string& adFile, const std::string& dataPath, const std::string& dataPath2): seq(adFile) {
+    int initRet = SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO);
+    if(initRet == 0) {
+        std::cerr<<"SDL sound and timer init'd successfully\n";
+    }
+    else {
+        std::cerr<<"Error init'ing audio/timer subsystem: "<<SDL_GetError()<<"\n";
+    }
+
+    SDL_AudioSpec want;
+    want.freq=48000;
+    want.format=AUDIO_S16;
+    want.channels=2;
+    want.silence=0;
+    want.samples=sampleChunkSize / stereoChannels;
+    want.size=0;
+    want.callback=NULL;
+    want.userdata=NULL;
+
+    SDL_AudioSpec got;
+    sdlDevId= SDL_OpenAudioDevice(NULL, 0, &want, &got, 0);
+    if(!sdlDevId) {
+        fprintf(stderr, "Error opening audio device: %s\n", SDL_GetError());
+        fprintf(stderr, "No audio device opened.\n");
+    }
+    else {
+        printf("Freq: %d format: %d (%d) ch: %d sil: %d samp: %d size: %d\n", got.freq, got.format, want.format, got.channels, got.silence, got.samples, got.size);
+    }
+
+    SDL_PauseAudioDevice(sdlDevId, 0);
+    cbs.sdlDevId = sdlDevId;
+    cbs.seq = &seq;
+    SDL_AddTimer(8, AudioCallback, &cbs);
+}
+
+// Stop current queue, play music
+void audioManager::playMusic(const std::string& xmiFile){
+    seq.loadXmi(xmiFile);
+}
+
+void audioManager::playWav(const std::string& vocFile){}
+void audioManager::playSfx(int number){
+    seq.playSfx(number);
+}
+void audioManager::enqueueSong(const std::string& xmiFile){}
+void audioManager::clearQueue(){}
+void audioManager::startMusic(){}
+void audioManager::stopMusic(){}
+void audioManager::muteMusic(){}
+void audioManager::restartMusic(){}
+void audioManager::setVol(int level){}
+
+Uint32 AudioCallback(Uint32 interval, void* params) {
+    audioManager::callbackStruct* cbs = reinterpret_cast<audioManager::callbackStruct*>(params);
+    std::vector<int16_t> samples = cbs->seq->tick();
+    SDL_QueueAudio(cbs->sdlDevId, samples.data(), samples.size() * sizeof(int16_t));
+    return interval;
+}
--- audio/audioManager.h
+++ audio/audioManager.h
@@ -1,19 +1,37 @@
-#include<std::string>
+#include<string>
+#include<vector>
+#include "oplSequencer.h"
+#include "opl/opl.h"
 
 class audioManager {
 public:
-    audioManager(std::string& adFile, std::string& dataPath, std::string& dataPath2);
-    void playMusic(std::string& xmiFile); // Stop current queue, play music
-    void playWav(std::string& vocFile);
+    audioManager(const std::string& adFile, const std::string& dataPath, const std::string& dataPath2);
+    void playMusic(const std::string& xmiFile); // Stop current queue, play music
+    void playWav(const std::string& vocFile);
     void playSfx(int number);
-    void enqueueSong(std::string& xmiFile);
+    void enqueueSong(const std::string& xmiFile);
     void clearQueue();
-    void startMusic()
-    void stopMusic()
-    void muteMusic()
-    void restartMusic()
+    void startMusic();
+    void stopMusic();
+    void muteMusic();
+    void restartMusic();
     void setVol(int level);
+
+    struct callbackStruct {
+        int sdlDevId;
+        oplSequencer* seq;
+    } cbs;
 private:
     int volumeLevel;
-    superOpl opl;
+    int nextDelay;
+    std::vector<std::string> musicQueue;
+    oplSequencer seq;
+    std::mutex oplMutex;
+    int sdlDevId;
+
+    static const int stereoChannels = 2;
+    static const int sampleRate = OPL_SAMPLE_RATE;
+    static const int processPerSecond = 120;
+    static const int sampleChunkSize = (sampleRate * stereoChannels) / processPerSecond;
+    static const int sampleChunkTimeInMs = 1000 / processPerSecond;
 };
